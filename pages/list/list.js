1dbb985c41edaa6b58809ef840d3aa66aeb21ab1
ITDragonBlog <1625402590@qq.com>
2017-03-05 23:19:42 +0800

    for and template

---
+++ pages/list/list.js
@@ -0,0 +1,26 @@
+// pages/list/list.js
+Page({
+  data:{
+    msgList:[
+      {id:1,title:"标题一",time:"2017-3-5 23:01:59",src:"../../images/wechatHL.png"},
+      {id:2,title:"标题二",time:"2017-3-5 23:02:59",src:"../../images/wechatHL.png"},
+      {id:3,title:"标题三",time:"2017-3-5 23:03:59",src:"../../images/wechatHL.png"},
+      {id:4,title:"标题四",time:"2017-3-5 23:04:59",src:"../../images/wechatHL.png"}
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
