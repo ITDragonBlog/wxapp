4b5d41c7486349bdd3eb0e53624466223b87e46b
ITDragon <1625402590@qq.com>
2017-03-06 14:43:13 +0800

    UI

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,6 +1,13 @@
 // pages/detail/detail.js
 Page({
-  data:{},
+  data:{
+    msgDetail:[
+      {id:1,title:"标题一",time:"2017-3-5 23:01:59",src:"../../images/1.jpg",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+      {id:2,title:"标题二",time:"2017-3-5 23:02:59",src:"../../images/2.jpg"},
+      {id:3,title:"标题三",time:"2017-3-5 23:03:59",src:"../../images/3.jpg"},
+      {id:4,title:"标题四",time:"2017-3-5 23:04:59",src:"../../images/1.gif"}
+    ]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
