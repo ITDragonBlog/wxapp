dbc384e0f526b2eef5d7483ee78aae0b35cc91ce
ITDragon <1625402590@qq.com>
2017-03-11 16:19:26 +0800

    美化搜索框

--- pages/list/list.js
+++ pages/list/list.js
@@ -39,6 +39,7 @@ Page({
     inputVal: "",
     searchLogShowed: false
   },
+
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     var that = this;
@@ -52,12 +53,14 @@ Page({
     });
     loadMsgData(that);
   },
+
   onReady:function(){
     // 页面渲染完成
   },
   onShow:function(){
     // 页面显示
   },
+
   // 下拉刷新数据
   pullDownRefresh: function() {
     var that = this;
@@ -80,8 +83,8 @@ Page({
     that.setData({
       scrollTop : event.detail.scrollTop
     });
- },
- showInput: function () {
+  },
+  showInput: function () {
     var that = this;
     that.setData({
         inputShowed: true
