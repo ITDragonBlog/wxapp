f01d572b8082b0507076494a7e8454f5658f1333
ITDragon <1625402590@qq.com>
2017-03-11 16:12:10 +0800

    搜索

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,9 +1,8 @@
 // pages/list/list.js
 var app = getApp();
 
-// 当前页数
-var pageNum = 1;
-
+var pageNum = 1; // 当前页数
+var searchTitle = ""; // 搜索关键字
 
 // 加载数据
 var loadMsgData = function(that){
@@ -12,7 +11,9 @@ var loadMsgData = function(that){
   });
   var allMsg = that.data.msgList;
   app.ajax.req('/itdragon/findAll',{
-    "page":pageNum , "pageSize" : 6
+    "page":pageNum , 
+    "pageSize" : 6 ,
+    "search_LIKE_title" : searchTitle
   },function(res){  
     // 不能直接 allMsg.push(res); 相当于list.push(list);打乱了结构
     for(var i = 0; i < res.length; i++){
@@ -33,7 +34,10 @@ Page({
     msgList:[],
     hidden:true,
     scrollTop : 0,
-    scrollHeight:0
+    scrollHeight:0,
+    inputShowed: false,
+    inputVal: "",
+    searchLogShowed: false
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -77,6 +81,44 @@ Page({
       scrollTop : event.detail.scrollTop
     });
  },
+ showInput: function () {
+    var that = this;
+    that.setData({
+        inputShowed: true
+    });
+  },
+  // 点击 搜索 按钮后 隐藏搜索记录，并加载数据
+  searchData: function () {
+    var that = this;
+    that.setData({
+        msgList : [],
+        scrollTop : 0,
+        searchLogShowed: false
+    });
+    pageNum = 1;
+    loadMsgData(that);
+  },
+  // 点击叉叉icon 清除输入内容，同时清空关键字，并加载数据
+  clearInput: function () {
+    var that = this;
+    that.setData({
+        msgList : [],
+        scrollTop : 0,
+        inputVal: ""
+    });
+    searchTitle = "";
+    pageNum = 1;
+    loadMsgData(that);
+  },
+  // 输入内容时 把当前内容赋值给 查询的关键字，并显示搜索记录
+  inputTyping: function (e) {
+    var that = this;
+    that.setData({
+        inputVal: e.detail.value,
+        searchLogShowed: true
+    });
+    searchTitle = e.detail.value;
+  },
   onHide:function(){
     // 页面隐藏
   },
