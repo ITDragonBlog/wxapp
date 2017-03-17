2a440e8f220fade9fe849247a582fc03c4f2340d
ITDragon <1625402590@qq.com>
2017-03-17 18:24:56 +0800

    注册页面验证

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,6 +1,11 @@
+var util = require('../../utils/util.js');
+var app = getApp();
+var password = "";
 
 Page({
   data: {
+    showTopTips: false,
+    errorMsg: ""
   },
   onLoad: function () {
     var that = this;
@@ -12,5 +17,41 @@ Page({
         })
       }
     });
+  },
+  // 判断账号是否为空和判断该账号名是否被注册
+  checkAccount: function (e) {
+    var that = this;
+    var account = e.detail.value;
+    if ("" == util.trim(account)) {
+      util.isError("账号不能为空", that);
+    } else {
+      util.clearError(that);
+      app.ajax.req('/register/checkLoginName', {
+        "loginName": account
+      }, function (res) {
+        if (!res) {
+          util.isError("账号已经被注册过", that);
+        }
+      });
+    }
+  },
+  getPassword: function (e) {
+    var that = this;
+    password = e.detail.value;
+    if ("" == util.trim(password)) {
+      util.isError("密码不能为空", that);
+    } else {
+      util.clearError(that);
+    }
+  },
+  checkPassword: function (e) {
+    var that = this;
+    var subPassword = e.detail.value;
+    if (subPassword != password) {
+      util.isError("输入密码不一致", that);
+    } else {
+      util.clearError(that);
+    }
   }
+
 })
